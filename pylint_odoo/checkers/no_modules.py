c084fedec068876bf0df38790bd8d0a392473b08
Yurii Cherniavskyi <ych@tenderp.com>
2017-10-18 16:09:46 +0300

    Update manifest required author

--- pylint_odoo/checkers/no_modules.py
+++ pylint_odoo/checkers/no_modules.py
@@ -216,7 +216,7 @@ ODOO_MSGS = {
 }
 
 DFTL_MANIFEST_REQUIRED_KEYS = ['license']
-DFTL_MANIFEST_REQUIRED_AUTHOR = 'Odoo Community Association (OCA)'
+DFTL_MANIFEST_REQUIRED_AUTHOR = 'Tend ERP'
 DFTL_MANIFEST_DEPRECATED_KEYS = ['description']
 DFTL_LICENSE_ALLOWED = [
     'AGPL-3', 'GPL-2', 'GPL-2 or any later version',
